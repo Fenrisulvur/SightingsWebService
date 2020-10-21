352c57e08183993b2268ab5e0ba2c2ec840cf4c0
Corey Fults <coreyfults@gmail.com>
2020-10-21 14:42:16 -0400

    Tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import countries from "./countries";
 
-function App() {
+export default function App() {
+  const [name, setName] = React.useState("");
+  const [sighting, setSighting] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [country, setCountry] = React.useState("");
+
+  const handleSubmit = event => {
+    console.log(`
+      Name: ${name}
+      Sighting: ${sighting}
+      Description: ${description}
+      Country: ${country}
+    `);
+
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <form onSubmit={handleSubmit}>
+      <h1>Sighting Report</h1>
+      <label>
+        Name:
+        <input
+          name="name"
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        Type of sighting:
+        <select name="sighting" onChange={e => setSighting(e.target.value)} required>
+          <option value="select">Select an Option</option>
+          <option value="ufo">UFO</option>
+          <option value="creature">Creature</option>
+          <option value="meteor">Meteor</option>
+        </select>
+      </label>
+
+      <label>
+        Description:
+        <input
+          name="text"
+          type="text"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        Country:
+        <select
+          name="country"
+          value={country}
+          onChange={e => setCountry(e.target.value)}
+          required
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option key="" />
+          {countries.map(country => (
+            <option key={country}>{country}</option>
+          ))}
+        </select>
+      </label>
+
+      <button>Submit</button>
+    </form>
   );
 }
-
-export default App;
