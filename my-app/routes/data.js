41f64e8caef0b2cae71c3449786b6e1e7e6e8f77
Corey Fults <coreyfults@gmail.com>
2020-10-22 18:45:50 -0400

    Implemented data storing
    reads and writes .json file to store data from POST & GET requests.

--- my-app/routes/data.js
+++ my-app/routes/data.js
@@ -1,20 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
+
+
 
 router.get('/sighting_data', function (req, res, next) {
-    res.json({
-      "data":[
-        {"Name": "Test", "Sighting": "UFO", "Description": "TEST DESCRIPTION", "Country": "Romania"},
-        {"Name": "Test2", "Sighting": "Creature", "Description": "Coochie", "Country": "Germany"},
-      ]
-    });
+  // Read users.json file
+  try {
+    console.log("Get request received");
+    let rawdata = fs.readFileSync('sightingdata.json'); //read raw file
+    let parseddata = JSON.parse(rawdata); //parse into JSON
+    res.json(parseddata); //send parsed JSON
+  } catch (err) {
+    console.log(err.message);
+  }
+
 });
 
 router.post('/sighting_data', function (req, res, next) {
-    console.log("Received data "+typeof req.body);
-    var data = req.body;
-    console.log(data.Name+" "+data.Sighting+" "+data.Description+" "+data.Country);
+    try {
+      console.log("Received data");
+      var data = req.body; //Get the data from the request body
+      //console.log(data.Name+" "+data.Sighting+" "+data.Description+" "+data.Country);
+      let rawdata = fs.readFileSync('sightingdata.json'); //read raw file
+      let parseddata = JSON.parse(rawdata); //parse into JSON
+      //push new entry to the JSON "data" array
+      parseddata['data'].push({"Name": data.Name, "Sighting": data.Sighting, "Description": data.Description, "Country": data.Country})
 
+      let datastring = JSON.stringify(parseddata); //turn JSON into string
+      fs.writeFileSync('sightingdata.json', datastring); //write updated data to file
+    } catch (err) {
+      console.log(err.message);
+    }
     res.json();
 });
 
